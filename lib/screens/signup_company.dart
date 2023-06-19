c8194be02ba6fb522ef14172c3f2bce8beebe8f7
ishud <ishudev2021@gmail.com>
2023-06-19 23:11:16 +0530

    small bug fix in signup screen

--- lib/screens/signup_company.dart
+++ lib/screens/signup_company.dart
@@ -24,9 +24,7 @@ class _SignUpComScreenState extends State<SignUpComScreen> {
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-            color: Colors.white24
-        ),
+        decoration: BoxDecoration(color: Colors.white24),
         child: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.fromLTRB(
@@ -34,11 +32,12 @@ class _SignUpComScreenState extends State<SignUpComScreen> {
             child: Column(
               children: <Widget>[
                 Container(
-                  child: Text("Create an Account",
-                    style: TextStyle(color: Colors.indigo[900],
+                  child: Text(
+                    "Create an Account",
+                    style: TextStyle(
+                        color: Colors.indigo[900],
                         fontWeight: FontWeight.w500,
                         fontSize: 30),
-
                   ),
                 ),
                 const SizedBox(
@@ -47,34 +46,46 @@ class _SignUpComScreenState extends State<SignUpComScreen> {
                 ListTile(
                   title: Row(
                     children: <Widget>[
-                      Expanded(child: OutlinedButton(onPressed: () {
-                        UserType = "Student";
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const SignUpStuScreen()),
-                        );
+                      Expanded(
+                          child: FilledButton(
+                        onPressed: () {
+                          UserType = "Student";
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignUpStuScreen()),
+                          );
                         },
                         child: Text("Student"),
                         style: OutlinedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.indigo[900],
-                          side: BorderSide(color: Colors.indigo.shade900, width: 1),
-                          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-                        ),
-                      )
-                      ),
-                      SizedBox(width: 10,),
-                      Expanded(child: FilledButton(onPressed: () {
-                        UserType = "Company";
-                      },child: Text("Company"),
-                        style: OutlinedButton.styleFrom(
                           foregroundColor: Colors.indigo[900],
                           backgroundColor: Colors.white,
-                          side: BorderSide(color: Colors.indigo.shade900, width: 1),
-                          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                          side: BorderSide(
+                              color: Colors.indigo.shade900, width: 1),
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
                         ),
-                      )
-                      )
+                      )),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                          child: OutlinedButton(
+                        onPressed: () {
+                          UserType = "Company";
+                        },
+                        child: Text("Company"),
+                        style: OutlinedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.indigo[900],
+                          side: BorderSide(
+                              color: Colors.indigo.shade900, width: 1),
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                        ),
+                      )),
                     ],
                   ),
                 ),
@@ -87,62 +98,58 @@ class _SignUpComScreenState extends State<SignUpComScreen> {
                   alignment: Alignment.topLeft,
                   child: Text(
                     "Your Name",
-                    style: TextStyle(color: Colors.indigo.shade900,
+                    style: TextStyle(
+                        color: Colors.indigo.shade900,
                         fontSize: 15,
-                        fontWeight: FontWeight.w500
-                    ),
+                        fontWeight: FontWeight.w500),
                     textAlign: TextAlign.left,
                   ),
                 ),
-                reusableTextField("Enter Name", false,
-                    _emailTextController),
+                reusableTextField("Enter Name", false, _emailTextController),
                 const SizedBox(
                   height: 20,
                 ),
-                Row(
-                    children: <Widget>[
-                      Container(
-                        //color: Colors.black,
-                        width: MediaQuery.of(context).size.width * 0.46,
-                        height: 25,
-                        //alignment: Alignment.topLeft,
-                        child: Text(
-                          "Company Name",
-                          style: TextStyle(color: Colors.indigo.shade900,
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                      Container(
-                        //color: Colors.pink,
-                        width: MediaQuery.of(context).size.width * 0.4,
-                        height: 25,
-                        //alignment: Alignment.topLeft,
-                        child: Text(
-                          "Designation",
-                          style: TextStyle(color: Colors.indigo.shade900,
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                    ]
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: reusableTextField("Enter Company", false,
-                          _emailTextController),
+                Row(children: <Widget>[
+                  Container(
+                    //color: Colors.black,
+                    width: MediaQuery.of(context).size.width * 0.46,
+                    height: 25,
+                    //alignment: Alignment.topLeft,
+                    child: Text(
+                      "Company Name",
+                      style: TextStyle(
+                          color: Colors.indigo.shade900,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500),
+                      textAlign: TextAlign.left,
                     ),
-                    SizedBox(width: 10),
-                    Expanded(
-                        child:reusableTextField("Enter Designation", false,
-                        _emailTextController),
+                  ),
+                  Container(
+                    //color: Colors.pink,
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: 25,
+                    //alignment: Alignment.topLeft,
+                    child: Text(
+                      "Designation",
+                      style: TextStyle(
+                          color: Colors.indigo.shade900,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500),
+                      textAlign: TextAlign.left,
                     ),
-                  ]),
+                  ),
+                ]),
+                Row(children: <Widget>[
+                  Expanded(
+                    child: reusableTextField(
+                        "Enter Company", false, _emailTextController),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: reusableTextField(
+                        "Enter Designation", false, _emailTextController),
+                  ),
+                ]),
                 const SizedBox(
                   height: 20,
                 ),
@@ -152,24 +159,23 @@ class _SignUpComScreenState extends State<SignUpComScreen> {
                   alignment: Alignment.topLeft,
                   child: Text(
                     "Password",
-                    style: TextStyle(color: Colors.indigo.shade900,
+                    style: TextStyle(
+                        color: Colors.indigo.shade900,
                         fontSize: 15,
-                        fontWeight: FontWeight.w500
-                    ),
+                        fontWeight: FontWeight.w500),
                     textAlign: TextAlign.left,
                   ),
                 ),
-                reusableTextField("Enter Password", true,
-                    _passwordTextController),
+                reusableTextField(
+                    "Enter Password", true, _passwordTextController),
                 const SizedBox(
                   height: 20,
                 ),
-
                 firebaseUIButton(context, "Sign Up", () {
                   FirebaseAuth.instance
                       .signInWithEmailAndPassword(
-                      email: _emailTextController.text,
-                      password: _passwordTextController.text)
+                          email: _emailTextController.text,
+                          password: _passwordTextController.text)
                       .then((value) {
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context) => HomeScreen()));
@@ -199,11 +205,11 @@ class _SignUpComScreenState extends State<SignUpComScreen> {
           },
           child: const Text(
             " Sign In",
-            style: TextStyle(color: Colors.orangeAccent, fontWeight: FontWeight.bold),
+            style: TextStyle(
+                color: Colors.orangeAccent, fontWeight: FontWeight.bold),
           ),
         )
       ],
     );
   }
-
 }
