c988efc045b94b4b686c8b8a0fcb7b06313b3ddf
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2023-07-25 11:45:16 +0530

    cleaned the code of skill verification

--- lib/screens/profile_stu/skills/cert-verify.dart
+++ lib/screens/profile_stu/skills/cert-verify.dart
@@ -6,7 +6,6 @@ import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
 import 'package:html/dom.dart' as dom;
 import '../../../utils/color_utils.dart';
-import 'dart:convert';
 import 'add_skills.dart';
 
 class CertificateVerification extends StatefulWidget {
@@ -22,78 +21,17 @@ class _CertificateVerificationState extends State<CertificateVerification> {
   String skill = "";
   String username = "";
 
-  Future<void> verifySkill() async {
-    const String baseUrl = 'https://api.github.com';
-    final String userReposEndpoint = '/users/$username/repos';
-
-    try {
-      final response = await http.get(Uri.parse(baseUrl + userReposEndpoint));
-      if (response.statusCode == 200) {
-        final List<dynamic> reposData = json.decode(response.body);
-        List<String> usedLanguages = [];
-
-        for (var repo in reposData) {
-          if (repo['language'] != null) {
-            usedLanguages.add(repo['language']);
-          }
-        }
-
-        // Check if Flutter (Dart) is among the used languages
-        if (usedLanguages.contains(skill) || (skill == 'Flutter' && usedLanguages.contains('Dart'))) {
-          QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-              .collection("Users")
-              .doc(FirebaseAuth.instance.currentUser?.uid)
-              .collection('skills')
-              .where('skill', isEqualTo: skill)
-              .get();
-          if (querySnapshot.docs.isNotEmpty && response.body.contains(skill)) {
-            // Get the first document (you can choose how to handle multiple matches)
-            DocumentSnapshot docSnapshot = await querySnapshot.docs[0];
-
-            // Step 2: Use the `update` method to modify the document's fields
-            docSnapshot.reference.update({
-              'isVerified': true,
-              // Add other fields and their updated values here
-            });
-            // ignore: use_build_context_synchronously
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const AddSkill()),
-            );
-          }
-          print('User $username has Flutter-related repositories.');
-        } else {
-          // ignore: use_build_context_synchronously
-          Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const AddSkill()),
-            );
-          print(
-              'User $username does not have any Flutter-related repositories.');
-        }
-      } else {
-        print(
-            'Failed to fetch user repositories. Status code: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Error occurred while fetching user repositories: $e');
-    }
-  }
-
   Future<void> _makeSecureRequest() async {
     try {
       final http.Response response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
-        // Request was successful, handle the response here
         QuerySnapshot querySnapshot = await FirebaseFirestore.instance
             .collection("Users")
             .doc(FirebaseAuth.instance.currentUser?.uid)
             .collection('skills')
             .where('skill', isEqualTo: skill)
             .get();
-        final dom.Document document = parser.parse(response.body);
-        final String content = document.body?.text ?? '';
         if (querySnapshot.docs.isNotEmpty && response.body.contains(skill)) {
           DocumentSnapshot docSnapshot = await querySnapshot.docs[0];
           docSnapshot.reference.update({
@@ -116,10 +54,9 @@ class _CertificateVerificationState extends State<CertificateVerification> {
   @override
   Widget build(BuildContext context) {
     skill = widget.skill;
-    print('hello');
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Certificate and Project Verfication"),
+          title: const Text("Certificate Verfication"),
         ),
         body: Padding(
           padding: const EdgeInsets.all(18.0),
@@ -136,13 +73,11 @@ class _CertificateVerificationState extends State<CertificateVerification> {
             Padding(
               padding: const EdgeInsets.all(18.0),
               child: TextField(
-                // Customize the appearance and behavior of the TextField
                 decoration: const InputDecoration(
-                  labelText: 'certificate url', // Label text for the input
-                  border: OutlineInputBorder(), // Border around the input
+                  labelText: 'certificate url',
+                  border: OutlineInputBorder(),
                 ),
                 onChanged: (value) {
-                  // Callback function when the text input changes
                   setState(() {
                     url = value;
                   });
@@ -151,33 +86,6 @@ class _CertificateVerificationState extends State<CertificateVerification> {
             ),
             TextButton(
                 onPressed: _makeSecureRequest, child: const Text("Verify")),
-            const SizedBox(height: 50.0),
-            Text(
-              'Enter the username of your github profile:',
-              textAlign: TextAlign.left,
-              style: GoogleFonts.dmSans(
-                fontWeight: FontWeight.w700,
-                color: primarytheme,
-                fontSize: 20,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: TextField(
-                // Customize the appearance and behavior of the TextField
-                decoration: const InputDecoration(
-                  labelText: 'username', // Label text for the input
-                  border: OutlineInputBorder(), // Border around the input
-                ),
-                onChanged: (value) {
-                  // Callback function when the text input changes
-                  setState(() {
-                    username = value;
-                  });
-                },
-              ),
-            ),
-            TextButton(onPressed: verifySkill, child: const Text("Verify"))
           ]),
         ));
   }
